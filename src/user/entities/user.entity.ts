45d22f5a320824190e41efca4c798fe6f09551b2
유민식 <soule125@naver.com>
2024-03-13 18:06:18 +0900

    유저 엔티티 enum 파일 추가, 수정 및 createdAt 수정

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,43 @@
-export class User {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  Timestamp,
+} from 'typeorm';
+import { UserRole } from '../types/userRole.type';
+
+@Entity('User')
+export class User {
+  @PrimaryGeneratedColumn()
+  userId: number;
+
+  @Column({ type: 'varchar', length: 15 })
+  name: string;
+
+  @Column({ type: 'int', length: 3 })
+  age: number;
+
+  @Column({ type: 'varchar', length: 2 })
+  gender: string;
+
+  @Column({ type: 'varchar', length: 30, unique: true })
+  email: string; // 유니크 값
+
+  @Column({ type: 'varchar', length: 15, select: false }) // DBMS를 위해 명시해주는 부분
+  password: string; // TS를 위해 타입을 명시
+
+  @Column({ type: 'varchar', length: 13 })
+  phoneNumber: string;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  createdAt: Date;
+  // Timestamp; //  === unix time stamp(인간이 보기 힘든 시간)
+
+  @Column()
+  updatedAt: Timestamp;
+
+  //   @Column({ type: 'set', enum: UserRole, default: ['user'] })
+  @Column({ type: 'enum', default: [UserRole.USER] })
+  roles: UserRole[];
+}
