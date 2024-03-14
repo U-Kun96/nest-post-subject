7fe95d5839c05d3fc64fda028c5a71dca4014b80
유민식 <soule125@naver.com>
2024-03-14 11:32:38 +0900

    answer dto 중 답변 작성 부분 작성

--- src/answer/dto/create-answer.dto.ts
+++ src/answer/dto/create-answer.dto.ts
@@ -1,1 +1,7 @@
-export class CreateAnswerDto {}
+import { IsNotEmpty, IsString } from '@nestjs/class-validator';
+
+export class CreateAnswerDto {
+  @IsString()
+  @IsNotEmpty({ message: '내용을 입력해주세요' })
+  content: string;
+}
